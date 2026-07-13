53cfa0cb8c0e2715a1048938d7060074ba9e0d7e
Shubhangini <shubhanginipatil25@gmail.com>
2026-07-13 17:31:12 +0530

    Debug routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,12 +15,12 @@ db();
 console.log("Router mounted at /api/v1");
 
 app.use("/api/v1", router);
-// app.get("/test", (req, res) => {
-//   res.json({
-//     success: true,
-//     message: "Backend is working",
-//   });
-// });
+app.get("/test", (req, res) => {
+  res.json({
+    success: true,
+    message: "Backend is working",
+  });
+});
 
 app.listen(port, () => {
   console.log(`Serve running on port ${port}...`);

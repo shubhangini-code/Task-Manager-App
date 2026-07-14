35019937176aeff1142ea8b1f6643f0d1f5c8671
Shubhangini <shubhanginipatil25@gmail.com>
2026-07-14 07:59:20 +0530

    Test router

--- backend/src/routes/taskRoutes.js
+++ backend/src/routes/taskRoutes.js
@@ -1,20 +1,39 @@
+// import express from "express";
+// import {
+//   newTask,
+//   getTasks,
+//   updateTask,
+//   deleteTask,
+// } from "../controllers/taskControllers.js";
+
+// console.log("Task routes loaded");
+
+// const router = express.Router();
+// router.get("/hello", (req, res) => {
+//   res.json({ message: "Hello from router" });
+// });
+// router.post("/task", newTask);
+// router.get("/tasks", getTasks);
+// router.put("/task/:id", updateTask);
+// router.delete("/task/:id", deleteTask);
+
+// export default router;
+
 import express from "express";
-import {
-  newTask,
-  getTasks,
-  updateTask,
-  deleteTask,
-} from "../controllers/taskControllers.js";
+
+const router = express.Router();
 
 console.log("Task routes loaded");
 
-const router = express.Router();
 router.get("/hello", (req, res) => {
   res.json({ message: "Hello from router" });
 });
-router.post("/task", newTask);
-router.get("/tasks", getTasks);
-router.put("/task/:id", updateTask);
-router.delete("/task/:id", deleteTask);
+
+router.get("/tasks", (req, res) => {
+  res.json({
+    success: true,
+    message: "Tasks route is working",
+  });
+});
 
 export default router;
